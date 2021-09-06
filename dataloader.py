2ca3e67582ec8a51c527fe8e27f5f6b34af99921
Eric <843251949@qq.com>
2021-09-07 00:18:34 +0900

    data

---
+++ dataloader.py
@@ -0,0 +1,27 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, DataLoader
+import numpy as np
+import math
+
+
+class WineDataset(Dataset):
+    def __init__(self):
+        xy = np.loadtxt('./wine.csv', delimiter = ',', dtype = np.float32, skiprows = 1)
+        self.x = torch.from_numpy(xy[:, 1:])
+        self.y = torch.from_numpy(xy[:,[0]])
+        self.n_samples = xy.shape[0]
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
+        
+    def __len__(self):
+        return self.n_samples
+if __name__ == '__main__':
+    dataset = WineDataset()
+    dataloader = DataLoader(dataset = dataset, batch_size = 4, shuffle=True, num_workers=4)
+    datatiter = iter(dataloader)
+    data = datatiter.next()
+    feature, labels = data
+
+    print(feature, labels)
